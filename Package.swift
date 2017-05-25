53005a3322c8a926a579a370caf40f34b5d43b87
Gustavo Perdomo <gperdomor@gmail.com>
2017-05-25 15:18:23 -0400

    fix for vapor 2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "BML",
     dependencies: [
-        .Package(url: "https://github.com/vapor/core.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/node.git", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/node.git", majorVersion: 2)
     ]
 )
